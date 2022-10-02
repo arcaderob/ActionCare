0aa9fc814731b9a20e32d8e601d78f0f36d229f7
arcaderob <gill@arcaderob.com>
2022-10-02 16:49:04 -0300

    Hardcode Task List

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskAdder}>
+        <Button
+          id="addTask"
+          title="Add Task"
+        >
+          Add Task
+        </Button>
+      </View>
+      <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>These are the tasks for today:</Text>
+
+          <View style={styles.items}>
+            <Task />
+            <Task />
+            <Task />
+            <Task />
+          </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +31,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+  },
+  taskAdder: {
+    paddingTop: 30,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  tasksWrapper: {
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
   },
 });
